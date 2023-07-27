1f9593456019fe10c6ddd7a259d8a0c415d0bdd5
jsur <julius.suominen@gmail.com>
2023-07-27 17:27:33 +0300

    test: use mockgen to mock db

--- api/server.go
+++ api/server.go
@@ -7,12 +7,12 @@ import (
 
 // Server serves HTTP requests for our simple bank
 type Server struct {
-	store  *db.Store
+	store  db.Store
 	router *gin.Engine
 }
 
 // NewServer creates http routes
-func NewServer(store *db.Store) *Server {
+func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
